8b2acdda9c7d9094736519f6321c7f3490c4613f
Felipe <fgarciamello@gmail.com>
2026-03-24 11:44:40 -0300

    feat:declaraçoes e variaveisJS

--- projeto1/src/css/js/script.js
+++ projeto1/src/css/js/script.js
@@ -1,1 +1,48 @@
-alert("Alerta Externo")
+// Declaraçoes e variaveis
+
+//var,let e const
+
+//var xpto =10;
+//console.log(xpto);
+//var xpto=20;
+//console.log(xpto);
+
+//var  - pode redeclarar e acessar fora do escopo
+
+//let nao pode redeclarar e nao consegue acessar fora escopo
+
+
+
+//if(true){
+//var nome="Fiap"
+
+//}
+
+//console.log(nome);
+
+//const
+//const taxaJuros = 0.85;
+//console.log(taxaJuros);
+
+var exemplo1="Ola dev";
+console.log(exemplo1);
+
+let exemplo2="Ola dev let";
+const exemplo3="ola dev const";
+console.log(exemplo3)
+
+let exemplo4=20;
+console.log(typeof exemplo4);
+
+let exemplo=true;
+console.log(typeof exemplo5);
+
+let exemplo6={};
+console.log(typeof exemplo6);
+
+let exemplo7=["Java" , "Python", "C#"];
+console.lof(typeof exemplo7);
+
+//variavel indefinida
+let exemplo8;
+console.log(typeof exemplo8)
